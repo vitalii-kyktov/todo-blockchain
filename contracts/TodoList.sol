64c453a2d6efa29c209fccecde8d34e28e2e7d8e
vitality <vitalii.kyktov@gmail.com>
2021-11-25 15:44:19 +0100

    adds tasks creation functionality to the TodoList.sol

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,23 @@
 pragma solidity ^0.8.0;
 
 contract TodoList {
+
+    constructor() public {
+        createTask("Buy milk");
+    }
+
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping (uint => Task) public tasks;
+
+    function createTask (string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
